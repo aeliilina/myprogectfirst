8b3c4f1dd23f4f64ad2e99e41e14eb307f013154
aeliilina <karybaevaaelina@gmail.com>
2024-04-15 17:19:39 +0600

    codifyblock

--- js/app.js
+++ js/app.js
@@ -12,3 +12,40 @@ mobileMenuClose.addEventListener('click', function () {
 	document.body.classList.remove('overflowHidden')
 })
 
+var servicesItems = document.querySelector(".servicesItems")
+
+var blockArr = [
+    {
+        img: 'assets/ic-construction.png',
+        text: 'Construction'
+    },
+    {
+        img: 'assets/ic-pantone.svg',
+        text: 'Project Development'
+    } ,
+	{
+        img: 'assets/ic-like.svg',
+        text: 'Interior Design'
+    },
+	{
+        img: 'assets/ic-plan.svg',
+        text: 'Repairs'
+    }
+]
+
+blockArr.forEach(function(item){
+    var block = document.createElement("div")
+    var img = document.createElement("img")
+    var text = document.createElement("p")
+	
+
+    img.setAttribute("src", item.img)
+    text.textContent = item.text
+    block.append(img)
+    block.append(text)
+
+	block.classList.add('block')
+
+	servicesItems.append(block)
+})
+
